64e6354f5bbab4cefaa4c4a5aff3b1f431f18dd0
Collapsar <dg8882219@gmail.com>
2024-02-28 01:21:44 +0800

    update flyio

--- ChatGPT_HKBU.py
+++ ChatGPT_HKBU.py
@@ -1,10 +1,12 @@
 import configparser
 import requests
+import os
 
 
 class HKBU_ChatGPT():
 
     def __init__(self, config_path='./config.ini'):
+        os.environ['ACCESS_TOKEN']
         if type(config_path) == str:
             self.config = configparser.ConfigParser()
             self.config.read(config_path)
@@ -13,12 +15,12 @@ class HKBU_ChatGPT():
 
     def submit(self, message):
         conversation = [{"role": "user", "content": message}]
-        url = (self.config['CHATGPT']['BASICURL']) +\
-            "/deployments/" + (self.config['CHATGPT']['MODELNAME']) +\
+        url = (os.environ['CHATGPT_BASICURL']) +\
+            "/deployments/" + (os.environ['CHATGPT_MODELNAME']) +\
             "/chat/completions/?api-version=" +\
-            (self.config['CHATGPT']['APIVERSION'])
+            (os.environ['CHATGPT_APIVERSION'])
         headers = {'Content-Type': 'application/json',
-                   'api-key': (self.config['CHATGPT']['ACCESS_TOKEN'])}
+                   'api-key': (os.environ['CHATGPT_ACCESS_TOKEN'])}
         payload = {'messages': conversation}
         response = requests.post(url, json=payload, headers=headers)
         if response.status_code == 200:
