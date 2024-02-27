fe494fe888498e854beec63118f931c96b0dbf3c
Collapsar <dg8882219@gmail.com>
2024-02-28 00:41:11 +0800

    update flyio

--- access.py
+++ access.py
@@ -2,4 +2,4 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 print(config['TELEGRAM']['ACCESS_TOKEN'])
-#Notice: Do not track and push this configuration file to public r
+# Notice: Do not track and push this configuration file to public r
